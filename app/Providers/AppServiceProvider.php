9553c72d0e1d772fce64f6126f5b60aafa3243d7
Ijas Ameen <191382018+ijasameen@users.noreply.github.com>
2025-10-31 19:46:08 +0530

    configure app defaults in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::prohibitDestructiveCommands(
+            app()->isProduction(),
+        );
+
+        Model::preventLazyLoading(! app()->isProduction());
+        Model::unguard();
+        Model::shouldBeStrict();
+
+        Password::defaults(function () {
+            return app()->isProduction()
+                ? Password::min(8)
+                    ->max(255)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised()
+                : null;
+        });
+
+        Date::use(CarbonImmutable::class);
     }
 }
